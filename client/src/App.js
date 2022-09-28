19460eacec93203aa325e01c9dd0095279d3036c
Dustin Lapierre <dustinlapierre@yahoo.com>
2022-09-28 14:48:51 +0200

    Imported styled-components to project and styled Landing page

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ function App()
 {
     return (
         <div>
-            <h1>Jobify</h1>
             <Landing />
         </div>
     );
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,14 +1,15 @@
 import logo from "../assets/images/logo.svg";
 import main from "../assets/images/main.svg";
+import styled from "styled-components";
 
 const Landing = () =>
 {
     return (
-        <main>
+        <Wrapper>
             <nav>
                 <img src={logo} alt="jobify" className="logo" />
             </nav>
-            <div className="container">
+            <div className="container page">
                 <div className="info">
                     <h1>
                         Job <span>Tracking</span> App
@@ -21,12 +22,52 @@ const Landing = () =>
                         suscipit adipiscing bibendum est ultricies integer.
                     </p>
                     <button className="btn btn-hero">Login/Register</button>
-                    <div>
-                        <img src={main} alt="job hunt" className="img main-img" />
-                    </div>
+                </div>
+                <div>
+                    <img src={main} alt="job hunt" className="img main-img" />
                 </div>
             </div>
-        </main>
+        </Wrapper>
     );
 };
+
+const Wrapper = styled.main`
+nav{
+    width: var(--fluid-width);
+    max-width: var(--max-width);
+    margin: 0 auto;
+    height: var(--nav-height);
+    display: flex;
+    align-items: center;
+}
+.page{
+    min-height: calc(100vh - var(--nav-height));
+    display: grid;
+    align-items: center;
+    margin-top: -3rem;
+}
+h1{
+    font-weight: 700;
+    span{
+        color: var(--primary-500);
+    }
+}
+p{
+    color: var(--grey-600);
+}
+.main-img{
+    display:none;
+}
+@media (min-width: 992px)
+{
+    .page{
+        grid-template-columns: 1fr 1fr;
+        column-gap: 3rem;
+    }
+    .main-img{
+        display: block;
+    }
+}
+`;
+
 export default Landing;
