07e36cd9d908f0f022c033fd2d18a5705df52fae
Dustin Lapierre <dustinlapierre@yahoo.com>
2022-09-30 14:15:53 +0200

    Created components for the different pages and routed them

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,15 @@
-import Landing from "./pages/Landing";
 import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
+import {Dashboard, Register, Landing, Error} from "./pages";
 
 function App()
 {
     return (
         <BrowserRouter>
-            <nav>
-                <Link to="/">Dashboard</Link>
-                <Link to="/register">Register</Link>
-                <Link to="/landing">Landing</Link>
-            </nav>
             <Routes>
-                <Route path="/" element={<div>DASHBOARD</div>} />
-                <Route path="/register" element={<div>REGISTER</div>} />
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/register" element={<Register />} />
                 <Route path="/landing" element={<Landing />} />
-                <Route path="*" element={<h1>404 NOT FOUND</h1>} />
+                <Route path="*" element={<Error />} />
             </Routes>
         </BrowserRouter>
     );
