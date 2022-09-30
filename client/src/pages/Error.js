2228546c90baf2c6cec16c9712987823492eae2a
Dustin Lapierre <dustinlapierre@yahoo.com>
2022-09-30 14:42:41 +0200

    Error page content added

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,7 +1,17 @@
+import {Link} from "react-router-dom";
+import img from "../assets/images/not-found.svg";
+import Wrapper from "../assets/wrappers/ErrorPage";
+
 const Error = () =>
 {
     return (
-        <div>Error</div>
+        <Wrapper className="full-page">
+            <div>
+                <img src={img} alt="not found" />
+                <h3>So ein Mist! Not Found!</h3>
+                <Link to="/">Return Home</Link>
+            </div>
+        </Wrapper>
     );
 };
 export default Error;
