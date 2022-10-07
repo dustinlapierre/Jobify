dec849b606869a559af5d33c46e9febd738c4eaf
Dustin Lapierre <dustinlapierre@yahoo.com>
2022-10-07 15:55:17 +0200

    Setup express routing

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 import express from "express";
-import dotenv from "dotenv";
-import connectDB from "./db/connect.js";
 const app = express();
+import dotenv from "dotenv";
 dotenv.config(); //loads environment variables from .env in root
+
+//db and auth
+import connectDB from "./db/connect.js";
+
+//routers
+import authRouter from "./routes/authRoutes.js";
 
 //middleware
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 import notFoundMiddleware from "./middleware/not-found.js";
 
+//parse incoming json
+app.use(express.json());
+
 app.get("/", (req, res) =>
 {
     res.send("Welcome!");
 });
+
+app.use("/api/v1/auth", authRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
