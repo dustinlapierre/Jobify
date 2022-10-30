c217bff5af7586289864370c13d75b6f6750fad8
Dustin Lapierre <dustinlapierre@yahoo.com>
2022-10-30 20:33:10 +0100

    Added duplicate email check in register controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,9 +5,17 @@ import {BadRequestError} from "../errors/index.js";
 const register = async (req, res, next) =>
 {
     const {name, email, password} = req.body;
+
     if(!name || !email || !password)
     {
         throw new BadRequestError("Please provide all values");
+    }
+
+    const userAlreadyExists = await User.findOne({email});
+
+    if(userAlreadyExists)
+    {
+        throw new BadRequestError("Email already in use!");
     }
 
     const user = await User.create({name, email, password});
